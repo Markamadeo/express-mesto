4f86869b981472bc5d7912d469ba7e5b639ad005
Artem Kulakov <kulakov22@yandex.ru>
2021-01-17 22:31:13 +0700

    add: static file

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import path from 'path';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+const __dirname = path.resolve();
 
+app.use(express.static(path.resolve(__dirname, 'public')));
 app.listen(PORT, () => { // eslint-disable-next-line no-console
   console.log(`Server has been started on port ${PORT}...`);
 });
