5e25e60f6498b13b14703c8c362e1ad6b9fd45d3
Artem Kulakov <kulakov22@yandex.ru>
2021-02-08 19:26:30 +0700

    feat: add card and user model

---
+++ models/card.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator(link) {
+        // eslint-disable-next-line no-useless-escape
+        return /^(https?:\/\/)?([\w\.]+)\.([a-z]{2,6}\.?)(\/[\w\.]*)*\/?\#?$/.test(link);
+      },
+      message: 'С вашей ссылкой что-то не так...',
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
+    likes: {
+      type: [mongoose.Schema.Types.ObjectId],
+      required: true,
+      default: undefined,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+});
+
+module.exports = mongoose.model('card', cardSchema);
